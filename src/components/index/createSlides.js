8f2fdf5f5d155d189dd0c7666f28cd9ec2cb7a2f
Doug Sauve <douglas_sauve@live.com>
2018-07-30 22:37:31 -0500

    small bug fix

--- src/components/index/createSlides.js
+++ src/components/index/createSlides.js
@@ -5,24 +5,33 @@ const image1m = <img className = "slide__image" src = './images/1-mobile.jpg'></
 const image2 = <img className = "slide__image" src = './images/2.jpg'></img>
 const image3 = <img className = "slide__image" src = './images/3.jpg'></img>
 
-const slides = [
-  [
-    [image1, image1m],
-    'Curious about what I can do?',
-    'Check out my work',
-    './app'
-  ], [
-    image2,
-    'Want my help with a project?',
-    'Get in touch',
-    './contact'
-  ], [
-    image3,
-    'See what\'s new on my blog.',
-    'Go',
-    './blog'
-  ]
-];
+class CreateSlides {
+  constructor() {
+    this.slides = [
+      [
+        [image1, image1m],
+        'Curious about what I can do?',
+        'Check out my work',
+        './app'
+      ], [
+        image2,
+        'Want my help with a project?',
+        'Get in touch',
+        './contact'
+      ], [
+        image3,
+        'See what\'s new on my blog.',
+        'Go',
+        './blog'
+      ]
+    ];
+
+    this.getSlides = () => {
+      return this.slides;
+    };
+  };
+};
 
 
-export default slides;
+
+export default CreateSlides;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import createSlides from '../components/index/createSlides';
+const CreateSlides = new createSlides();
 
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
@@ -8,24 +9,45 @@ import Footer from '../components/Footer';
 import IndexHeader from '../components/index/IndexHeader'
 import Slideshow from '../components/index/Slideshow';
 
-const Index = () => (
-  <div>
-    <IndexHeader content = {'Hi, I\'m Doug. I create web apps.'} />
-    <NavBar
-      dests = {[
-        ['Home', '/'],
-        ['About me', '/about'],
-        ['My work', '/app'],
-        ['Contact', '/contact'],
-        ['Blog', '/blog']
-      ]}
-    />
-    <Slideshow
-      slides = {createSlides}
-    />
+class Index extends React.Component{
 
-    <Footer />
-  </div>
-);
+  state = {
+    slides: null,
+  };
+
+  componentWillMount() {
+    this.loadSlides();
+  };
+
+  loadSlides = async () => {
+    const slides = await CreateSlides.getSlides();
+
+    this.setState(() => ({ slides }));
+  };
+
+  render() {
+    return (
+      <div>
+        <IndexHeader content = {'Hi, I\'m Doug. I create web apps.'} />
+        <NavBar
+          dests = {[
+            ['Home', '/'],
+            ['About me', '/about'],
+            ['My work', '/app'],
+            ['Contact', '/contact'],
+            ['Blog', '/blog']
+          ]}
+        />
+        {this.state.slides &&
+          <Slideshow
+            slides = {this.state.slides}
+          />
+        }
+
+        <Footer />
+      </div>
+    );
+  };
+};
 
 export { Index as default };
